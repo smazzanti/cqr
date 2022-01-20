60127f0f9eec5c377d2445a307d5fd1d2d8aab36
smazzanti <mazzanti.sam@gmail.com>
2022-01-20 23:52:57 +0100

    imports

--- cqr/__init__.py
+++ cqr/__init__.py
@@ -1,1 +1,7 @@
 from . import helper
+from . import nonconformist_acp
+from . import nonconformist_cp
+from . import nonconformist_evaluation
+from . import nonconformist_nc
+from . import nonconformist_base
+from . import nonconformist_icp
